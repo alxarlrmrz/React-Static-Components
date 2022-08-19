2db8cb8bf2b87c183fa95486264f7ddde63a7df2
alxarlrmrz <alxarlrmrz@gmail.com>
2022-08-19 13:38:41 -0400

    still wonky

--- app.js
+++ app.js
@@ -1,103 +1,113 @@
-class Sidebar extends React.Component {
+class Dashboard extends React.Component{
     render() {
-        return (
-            <div className="sidebar">
-                <h3>Dashboard</h3>
-                <h3>Widget</h3>
-                <h3>Reviews</h3>
-                <h3>Customers</h3>
-                <h3>Online Analysis</h3>
-                <h3>Settings</h3>
+        return(
+            <div className="dashboard">
+              <h3>Dashboard</h3>
+              <h3>Widget</h3>
+              <h3>Reviews</h3>
+              <h3>Customers</h3>
+              <h3>Online Analysis</h3>
+              <h3>Setting</h3>
             </div>
-        )
+        )        
     }
 }
 
 class Reviews extends React.Component {
-    render() {
-        return (
-            <div className="Reviews">
-                <h3>Reviews</h3>
-                <p>1,281</p>
-            </div>
-        )
+    render () {
+      return (
+        <div className="reviews">
+          <h3>Reviews</h3>
+          <p>1,281</p>
+        </div>
+      )
     }
-}
+  }
 
-class AvgRating extends React.Component {
-    render() {
-        return (
-            <div className="AvgRating">
-                <h3>Average Rating</h3>
-                <p>4.6</p>
-            </div>
-        )
+  class Rating extends React.Component {
+    render () {
+      return (
+        <div className="rate">
+          <h3>Average Rating</h3>
+          <p>4.6</p>
+        </div>
+      )
     }
-}
+  }
 
-class SentAn extends React.Component {
-    render() {
-        return (
-            <div className="SentAn">
-                <h3>Sentiment Analysis</h3>
-                <ul>
-                    <li>960</li>
-                    <li>122</li>
-                    <li>321</li>
-                </ul>
-            </div>
-        )
+  class Analysis extends React.Component {
+    render () {
+      return (
+        <div className="analysis">
+          <h3>Sentiment Analysis</h3>
+          <ul>
+          <li>960</li>
+          <li>122</li>
+          <li>321</li>
+          </ul>
+        </div>
+      )
     }
-}
+  }
 
-class WebVisitors extends React.Component {
+class Visitors extends React.Component{
     render() {
         return (
-            <div className="webVisitors">
+            <div className="visitors">
                 <h3>Website Visitors</h3>
                 <p>821</p>
-                <div className="webVisDiv"></div>
+                <div>
+                  <InfoDisplay />
+                </div>
             </div>
         )
     }
 }
 
-class Stats extends React.Component {
-    render() {
-        return (
-            <div className="stats">
-                <Reviews />
-                <AvgRating />
-                <SentAn />
-            </div>
-        )
-    }
+class InfoDisplay extends React.Component{
+  render() {
+      return (
+        <div className="info"></div>
+      )
+  }
 }
 
-class Content extends React.Component {
-    render() {
-        return (
-            <div className="content">
-                <Stats />
-                <WebVisitors />
-            </div>
-        )
-    }
+class Stats extends React.Component{
+  render() {
+      return (
+          <div className="stats">
+              <Reviews />
+              <Rating />
+              <Analysis />
+          </div>
+         
+      )
+  }
 }
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="app">
-                <Sidebar />
-                <Content />
-            </div>
-        )
-    }
+class Content extends React.Component{
+  render() {
+      return (
+          <div className="content">
+              <Stats />
+              <Visitors />
+          </div>
+      )
+  }
 }
 
+class App extends React.Component{
+  render() {
+      return (
+          <div className="container">
+              <Dashboard />
+              <Content />
+          </div>
+      )
+  }
+}
 
 ReactDOM.render(
     <App />,
-    document.querySelector('.main')
+    document.querySelector('.container')
 )
